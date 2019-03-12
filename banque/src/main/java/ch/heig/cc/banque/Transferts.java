fd978b490da7c7f7caec5522be0eb6ed94f7e7eb
Emerzit <44234101+Emerzit@users.noreply.github.com>
2019-03-12 17:24:44 +0100

    changed transfert

--- banque/src/main/java/ch/heig/cc/banque/Transferts.java
+++ banque/src/main/java/ch/heig/cc/banque/Transferts.java
@@ -17,22 +17,17 @@ public class Transferts {
         return randomNum;
     }
 
-    public boolean randomTransfert(final int nbTrans) {
+    public Thread randomTransfert(final int nbTrans) {
 
         Thread transThread = new Thread(new Runnable() {
             public void run() {
 
                 for (int i = 0; i < nbTrans; i++) {
                     banque.transfert(randInt(0, banque.getNbComptes()), randInt(0, banque.getNbComptes()), rand.nextInt());
-                    try {
-                        this.wait();
-                    } catch (InterruptedException e) {
-                        e.printStackTrace();
-                    }
                 }
             }
         });
-        return true;
+        return transThread;
     }
 
     public Banque getBanque() {
