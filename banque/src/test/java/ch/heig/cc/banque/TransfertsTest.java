8254ccce8b0f16315d9b44bd17ad5f9337856267
edechambrier <37554761+edechambrier@users.noreply.github.com>
2019-03-19 15:59:48 +0100

    teste boucle terminate

--- banque/src/test/java/ch/heig/cc/banque/TransfertsTest.java
+++ banque/src/test/java/ch/heig/cc/banque/TransfertsTest.java
@@ -2,6 +2,11 @@ package ch.heig.cc.banque;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class TransfertsTest {
@@ -9,8 +14,18 @@ class TransfertsTest {
     @Test
     void randomTransfert() {
         Transferts tr = new Transferts(new Banque(10));
-        for (int i = 0; i < 1000; i++) {
-            tr.randomTransfert(1000);
+        int iteration = 1000;
+        Thread threadList[] = new Thread[iteration];
+        for (int i = 0; i < iteration; i++) {
+            threadList[i] = tr.randomTransfert(iteration );
+        }
+
+        for(int i = 0; i< iteration; i++){
+            try {
+                threadList[i].join();
+            }catch (Exception e){
+                e.printStackTrace();
+            }
         }
         assertTrue(tr.getBanque().consistent());
     }
